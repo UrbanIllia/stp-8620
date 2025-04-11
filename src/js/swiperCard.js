440c3b735f1c85cc4ca3891db8c0126fe14877b3
Urban <urban.mutagen@gmail.com>
2025-04-11 10:42:00 +0300

    add secton without cards

---
+++ src/js/swiperCard.js
@@ -0,0 +1,44 @@
+// const swiper = new Swiper('.swiper', {
+//   effect: 'cards',
+//   cardsEffect: {
+//     perSlideRotate: 0,
+//     perSlideOffset: 20,
+//     slideShadows: false,
+//     rotate: false,
+//   },
+//   grabCursor: true,
+// });
+const swiper = new Swiper('.mySwiper', {
+  effect: 'cards',
+  cardsEffect: {
+    perSlideRotate: 0,
+    perSlideOffset: 20,
+    slideShadows: false,
+    rotate: false,
+  },
+  grabCursor: true,
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 0,
+  breakpoints: {
+    1200: {
+      direction: 'vertical',
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      cardsEffect: {
+        perSlideOffset: 30,
+      },
+    },
+
+    1201: {
+      direction: 'horizontal',
+      slidesPerView: 1,
+      spaceBetween: 0,
+      cardsEffect: {
+        perSlideOffset: 20,
+      },
+    },
+  },
+});
+
+swiper.update();
