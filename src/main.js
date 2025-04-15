4c84bea37478626c2122370c22961687a8cbce9f
lika_vil <125767690+Lika060@users.noreply.github.com>
2025-04-15 15:48:41 +0300

    final

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import './js/burgerMenu';
 import './js/header';
 
 import './js/swiperCard';
-<<<<<<< HEAD
 
 import './js/12CookiesModal';
-=======
+
 import './js/anchors';
->>>>>>> c9ef9ebebfaa0d3509a8de204bd0f69bdff11a7a
