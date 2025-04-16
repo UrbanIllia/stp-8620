5bdfe5891a22e47f5ea0264c16f4a4e4d977dccf
Urban <urban.mutagen@gmail.com>
2025-04-16 13:23:22 +0300

    fixed header and js for anchors

--- src/js/anchors.js
+++ src/js/anchors.js
@@ -1,20 +1,25 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href');
-    const targetElement = document.querySelector(targetId);
-    if (targetElement) {
-      const headerHeight = document.querySelector('.header').offsetHeight || 0;
-      const offset = window.innerWidth <= 1200 ? -40 : -88;
-      const targetPosition =
-        targetElement.getBoundingClientRect().top +
-        window.scrollY -
-        headerHeight -
-        offset;
-      window.scrollTo({
-        top: targetPosition,
-        behavior: 'smooth',
-      });
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        const header = document.querySelector('.header');
+        const headerHeight = header ? header.offsetHeight : 0;
+        const offset = window.innerWidth <= 1200 ? -34 : 38;
+        const targetPosition =
+          targetElement.getBoundingClientRect().top +
+          window.scrollY -
+          headerHeight -
+          offset;
+        window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth',
+        });
+      } else {
+        console.error(`Element with ID ${targetId} not found`);
+      }
+    });
   });
 });
